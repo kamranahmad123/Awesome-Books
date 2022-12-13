2b1dadcf023174b3bc991d6fa59580e54bcd2097
Kamran123Ahmad <kamrankhan.ahmad2@yahoo.com>
2022-12-12 17:39:01 -0800

    Create primary HTML page and JS structure

---
+++ javascript.js
@@ -0,0 +1,5 @@
+const form = document.querySelector(".form");
+const Button = document.querySelector(".Button");
+const title = document.querySelector(".title");
+const author = document.querySelector(".author");
+const books = document.querySelector(".books");
